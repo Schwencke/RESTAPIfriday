35f88fa9ce208564a9f2414f78ddd964ac58063f
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-09-16 19:39:33 +0200

    added entity and fields for dtos

--- src/main/java/dtos/PersonDTO.java
+++ src/main/java/dtos/PersonDTO.java
@@ -1,4 +1,53 @@
 package dtos;
 
 public class PersonDTO {
+    private String fName;
+    private String lName;
+    private String phone;
+    private Integer id;
+
+    public PersonDTO(String fName, String lName, String phone) {
+        this.fName = fName;
+        this.lName = lName;
+        this.phone = phone;
+    }
+
+    public PersonDTO(String fName, String lName, String phone, Integer id) {
+        this.fName = fName;
+        this.lName = lName;
+        this.phone = phone;
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
