581d7ed0c4e72bcbd072df431c4679de86360c45
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-09-16 19:30:30 +0200

    setup DTO, facade and interface

---
+++ src/main/java/facades/PersonFacade.java
@@ -0,0 +1,31 @@
+package facades;
+
+import dtos.PersonDTO;
+import dtos.PersonsDTO;
+
+public class PersonFacade implements IPersonFacade{
+    @Override
+    public PersonDTO addPerson(String fName, String lName, String phone) {
+        return null;
+    }
+
+    @Override
+    public PersonDTO deletePerson(int id) {
+        return null;
+    }
+
+    @Override
+    public PersonDTO getPerson(int id) {
+        return null;
+    }
+
+    @Override
+    public PersonsDTO getAllPersons() {
+        return null;
+    }
+
+    @Override
+    public PersonDTO editPerson(PersonDTO p) {
+        return null;
+    }
+}
