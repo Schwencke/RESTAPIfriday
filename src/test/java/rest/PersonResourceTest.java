10ce84edf4dc8ab1168621b72d68a434205cef3f
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-09-18 08:56:06 +0200

    testing stupid code

--- src/test/java/rest/PersonResourceTest.java
+++ src/test/java/rest/PersonResourceTest.java
@@ -64,7 +64,6 @@ public class PersonResourceTest {
     }
 
     // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
-    //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
     @BeforeEach
     public void setUp() {
 
